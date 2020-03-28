983df6b346e81d916bd67d9a98a62721aeea61aa
Rdeleon <rdeleonjr1007@yahoo.com>
2020-03-28 10:35:26 -0700

    Week $ Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,17 @@ $(function(){
         }
     });
 });
+
+$(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+
+});
+
+$(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+
+});
